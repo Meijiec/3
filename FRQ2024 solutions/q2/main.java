cbdb6e29b1f9cef5af563f95547c66024de18144
Meijiec <156829283+Meijiec@users.noreply.github.com>
2024-05-28 11:32:07 -0400

    Update main.java

--- FRQ2024 solutions/q2/main.java
+++ FRQ2024 solutions/q2/main.java
@@ -1,1 +1,24 @@
-
+public class Scoreboard {
+    private String team1Name;
+    private String team2Name;
+    private int team1Score;
+    private int team2Score;
+    private String activeTeam;
+    public Scoreboard(String team1Name, String team2Name) {
+        this.team1Name = team1Name;
+        this.team2Name = team2Name;
+        this.team1Score = 0;
+        this.team2Score = 0;
+        this.activeTeam = team1Name;
+    }
+    public void recordPlay(int points) {
+        if (points > 0) {
+            if (activeTeam.equals(team1Name)) {
+                team1Score += points;
+            } else {
+                team2Score += points;
+            }
+        } else {
+            switchActiveTeam();
+        }
+    }
