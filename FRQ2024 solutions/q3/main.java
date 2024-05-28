5ff228058bd5f91c51905ca30a0d24505f5c5bb6
Meijiec <156829283+Meijiec@users.noreply.github.com>
2024-05-28 11:40:14 -0400

    Update main.java

--- FRQ2024 solutions/q3/main.java
+++ FRQ2024 solutions/q3/main.java
@@ -1,4 +1,5 @@
-public boolean isWordChain(List<String> wordList) {
+A.
+    public boolean isWordChain(List<String> wordList) {
     if (wordList.size() < 2) {
         return false; 
     }
@@ -8,4 +9,15 @@ public boolean isWordChain(List<String> wordList) {
         }
     }
     return true;
+}
+
+B.
+    public ArrayList<String> createList(String target, List<String> wordList) {
+    ArrayList<String> newList = new ArrayList<>();
+    for (String word : wordList) {
+        if (word.startsWith(target)) {
+            newList.add(word.substring(target.length()));
+        }
+    }
+    return newList;
 }
