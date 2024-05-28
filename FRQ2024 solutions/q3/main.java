966c9e49d8f6697f5ac6d0a3416c642cd8431e42
Meijiec <156829283+Meijiec@users.noreply.github.com>
2024-05-28 11:37:11 -0400

    Update main.java

--- FRQ2024 solutions/q3/main.java
+++ FRQ2024 solutions/q3/main.java
@@ -1,1 +1,11 @@
-
+public boolean isWordChain(List<String> wordList) {
+    if (wordList.size() < 2) {
+        return false; 
+    }
+    for (int i = 1; i < wordList.size(); i++) {
+        if (!wordList.get(i).contains(wordList.get(i - 1))) {
+            return false;
+        }
+    }
+    return true;
+}
