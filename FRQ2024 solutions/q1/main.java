e6ece92a187e469a1f5bce9c1413d0069dffa618
Meijiec <156829283+Meijiec@users.noreply.github.com>
2024-05-28 11:28:09 -0400

    Update main.java

--- FRQ2024 solutions/q1/main.java
+++ FRQ2024 solutions/q1/main.java
@@ -1,1 +1,22 @@
+A.
+public void simulateOneDay(int numBirds) {
+    for (int i = 0; i < numBirds; i++) {
+        recordPlay(1);
+    }
+    if (Math.random() < 0.3) {
+       recordPlay(-5);
+    }
+}
 
+B.
+ public int simulateManyDays(int numBirds, int numDays) {
+    int daysWithFood = 0;
+    for (int i = 0; i < numDays; i++) {
+        simulateOneDay(numBirds);
+        if (team1Score > 0 || team2Score > 0) {
+            daysWithFood++;
+        }
+    }
+    return daysWithFood;
+}
+    
