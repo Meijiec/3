a7acc4d60de967081b48d38f045b24a1a6944733
Meijiec <156829283+Meijiec@users.noreply.github.com>
2024-05-28 11:45:25 -0400

    Update main.java

--- FRQ2024 solutions/q4/main.java
+++ FRQ2024 solutions/q4/main.java
@@ -1,1 +1,27 @@
+public Location getNextLoc(int row, int col) {
+    int nextRow = row + 1;
+    int nextCol = col + 1;
+    
+    if (nextRow <= maxRow && nextCol <= maxCol) {
+        Location downLoc = new Location(nextRow, col);
+        Location rightLoc = new Location(row, nextCol);
+        int downValue = getGridValue(downLoc);
+        int rightValue = getGridValue(rightLoc);
+    }
+     return downValue (rightValue  downLoc : rightLoc);  
+}
 
+
+
+B
+  public int sumPath(int row, int col) {
+    int sum = 0;
+    Location currentLoc = new Location(row, col);
+
+    while (currentLoc != null) {
+        sum += getGridValue(currentLoc); 
+        currentLoc = getNextLoc(currentLoc.getRow(), currentLoc.getCol());
+    }
+
+    return sum;
+}
